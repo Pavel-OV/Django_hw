4f0c04f4188a665f9553c5380e6ee1c97ec27498
Pavel <black_yaguar@mail.ru>
2024-02-06 13:37:12 +0500

    Создаем в файле views пару текстовых представлений

--- website/my_websiteapp/views.py
+++ website/my_websiteapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    logger.info("Cтраница index загрузилась")
+    return HttpResponse("<h1>Привет! Вы на моей страничке.</h1>")
+
+def about(request):
+    logger.info("Cтраница about загрузилась")
+    return HttpResponse("Страница обо мне")
