7835db8370df069d3b3a72741ca9714c256bf58b
Pavel <black_yaguar@mail.ru>
2024-02-15 16:37:23 +0500

    Файлы для создание, удаления  и чтения товаров

---
+++ website/online_storeapp/management/commands/delete_all_goods.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from online_storeapp.models import GoodsModel
+
+class Command(BaseCommand):
+    help = "Удаление всех продуктов"
+
+    def handle(self, *args, **kwargs):
+
+        products = GoodsModel.objects.all()
+
+        if products:
+            for product in products:
+                self.stdout.write(str(product))
+                product.delete()
+        else:
+            text: str = 'Продуктов нет в базе данных'
+            self.stdout.write(text)
