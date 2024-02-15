1649325e1b70e3df78db0a6e6cb3a768291c19f1
Pavel <black_yaguar@mail.ru>
2024-02-16 02:34:07 +0500

    Прописан путь для стриницы orders_by_days

--- website/online_storeapp/urls.py
+++ website/online_storeapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from online_storeapp.views import index, get_all_orders, order_list
+from online_storeapp.views import index, get_all_orders, order_list, orders_by_days
 
 urlpatterns = [
      path('', index, name='index'),
      path('orders/<int:name_client>/', get_all_orders, name='orders'),
      path('order/<int:order_id>/', order_list, name='order'),
+     path('orders_by_days/<int:buyer_id>/<int:count_day>', orders_by_days, name='orders_by_days'),
     ]
