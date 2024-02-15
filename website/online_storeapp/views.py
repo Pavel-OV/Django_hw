ad01079043d4f2480cfe1dfdd379fbebc11a8df6
Pavel <black_yaguar@mail.ru>
2024-02-16 02:35:46 +0500

    Функция  orders_by_days для поиска заказов по дням

--- website/online_storeapp/views.py
+++ website/online_storeapp/views.py
@@ -1,6 +1,8 @@
+
+from datetime import timedelta, timezone
 from django.shortcuts import render,  get_object_or_404
 from django.http import HttpResponse
-from online_storeapp.models import OrderstModel
+from online_storeapp.models import OrderstModel, ClientsModel
 
 
 def index(request):
@@ -17,9 +19,23 @@ def order_list(request: HttpResponse, order_id):
     order_id = OrderstModel.objects.get(pk=order_id).pk
     date_order = OrderstModel.objects.get(pk=order_id).date_of_order
     buyer = OrderstModel.objects.get(pk=order_id).buyer.name_client
-    price_total_order = OrderstModel.objects.get(pk=order_id).total_amount_of_order
+    price_total_order = OrderstModel.objects.get(
+        pk=order_id).total_amount_of_order
     products = OrderstModel.objects.get(pk=order_id).goods.all()
     return render(
         request, 'online_storeapp/order.html', {'order': order, 'order_id': order_id,
                                                 'date_order': date_order, 'products': products,
                                                 'buyer': buyer, 'price_total_order': price_total_order, })
+
+
+def orders_by_days(request: HttpResponse, buyer_id, count_day):
+    buyer = get_object_or_404(ClientsModel, pk=buyer_id)
+    orders = OrderstModel.objects.filter(buyer=buyer)
+    current_day =  timezone.now()
+    day_of_countdown = current_day - timedelta(days=count_day)
+    list_filter_orders = []
+    for order in orders:
+        if day_of_countdown <= order.date_of_order:
+            list_filter_orders.append(order)
+    return render(request, 'online_storeapp/orders_by_days.html',
+                  {'list_filter_orders': list_filter_orders, 'buyer': buyer, 'count_day': count_day, })
