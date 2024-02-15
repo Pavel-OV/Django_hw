6d9795e4c00c6ebbbf3ebb20deee143445a160f9
Pavel <black_yaguar@mail.ru>
2024-02-15 21:50:15 +0500

    Создание вьюшки

--- website/online_storeapp/views.py
+++ website/online_storeapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from django.http import HttpResponse
+from online_storeapp.models import OrderstModel
 
-# Create your views here.
+
+def index(request):
+    return render(request, "online_storeapp/index.html")
+
+
+def get_all_orders(request: HttpResponse, name_client):
+    orders = OrderstModel.objects.filter(buyer_id=name_client)
+    return render(request, 'online_storeapp/orders.html', {'orders': orders, 'buyer_id': name_client})
+
+
+def order_list(request: HttpResponse, order_id):
+    order = get_object_or_404(OrderstModel, pk=order_id)
+    order_id = OrderstModel.objects.get(pk=order_id).pk
+    date_order = OrderstModel.objects.get(pk=order_id).date_of_order
+    buyer = OrderstModel.objects.get(pk=order_id).buyer.name_client
+    price_total_order = OrderstModel.objects.get(pk=order_id).total_amount_of_order
+    products = OrderstModel.objects.get(pk=order_id).goods.all()
+    return render(
+        request, 'online_storeapp/order.html', {'order': order, 'order_id': order_id,
+                                                'date_order': date_order, 'products': products,
+                                                'buyer': buyer, 'price_total_order': price_total_order, })
