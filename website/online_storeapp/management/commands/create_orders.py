433d50b3cb6197eb642978bf97631a8c34021c84
Pavel <black_yaguar@mail.ru>
2024-02-15 21:49:02 +0500

    Оформление файла

--- website/online_storeapp/management/commands/create_orders.py
+++ website/online_storeapp/management/commands/create_orders.py
@@ -6,17 +6,16 @@ from decimal import Decimal
 
 
 class Command(BaseCommand):
+    help = 'Создание тестовых заказов'
+
     def handle(self, *args, **kwargs):
         buyers = ClientsModel.objects.all()
         goods = GoodsModel.objects.all()
         for i in range(1, 150):
             buyer = random.choice(buyers)
             order = OrderstModel.objects.create(buyer=buyer)
-           
-            
             name_product = random.choice(goods)
             order.goods.add(name_product)
-               
             order.total_amount_of_order = name_product.price_of_product
             order.date_of_order = timezone.now()
             order.save()
