e59ed91a951c9deeb39084b8abaab629bbd3760d
Pavel <black_yaguar@mail.ru>
2024-02-15 16:32:15 +0500

    Файлы для создание и чтения клиентов

--- website/online_storeapp/management/commands/create_clients.py
+++ website/online_storeapp/management/commands/create_clients.py
@@ -1,14 +1,15 @@
 from django.core.management.base import BaseCommand
 from online_storeapp.models import ClientsModel
+from random import randint
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        for i in range(0, 9):
+        for i in range(1, 250):
             client = ClientsModel(
                 name_client=f'Покупатель {i}',
                 email=f'email{i}@mail.ru',
-                phone=f'99{i}8{i}54{i}2{i}',
+                phone=f'9{randint(100000000, 999999999)}',
                 customer_address=f'Город{i}, улица{i}, дом{i}'
             )
             client.save()
