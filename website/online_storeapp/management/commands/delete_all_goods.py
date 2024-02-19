b1277fcdd9a48d4efe5cdd2e63cfdadd58a47de1
Pavel <black_yaguar@mail.ru>
2024-02-19 20:00:08 +0500

    Редактирование и создание команд для удаления товара и группы товаров

--- website/online_storeapp/management/commands/delete_all_goods.py
+++ website/online_storeapp/management/commands/delete_all_goods.py
@@ -1,13 +1,12 @@
 from django.core.management.base import BaseCommand
 from online_storeapp.models import GoodsModel
 
+
 class Command(BaseCommand):
     help = "Удаление всех продуктов"
 
     def handle(self, *args, **kwargs):
-
         products = GoodsModel.objects.all()
-
         if products:
             for product in products:
                 self.stdout.write(str(product))
