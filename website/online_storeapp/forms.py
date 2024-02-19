c290c3903c325678022491c5bec422c58d700182
Pavel <black_yaguar@mail.ru>
2024-02-19 20:05:36 +0500

    Создание формы для поиска по дням, наброски

--- website/online_storeapp/forms.py
+++ website/online_storeapp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import GoodsModel
+from .models import GoodsModel, OrderstModel
 
 class NewProduct(forms.ModelForm):
     class Meta:
         model = GoodsModel
         fields = ['name_product','description_product','price_of_product','quantity_of_product', 'image_product']
+
+
+class Buyer_Order_Date(forms.Form):
+    num_id = forms.IntegerField()
+    choice = forms.ChoiceField(choices=[('Неделя',7),('Месяц',30),('Год',365)])
