2bba31b58c56f1f0defbbf3f29ef7dcee3cbf174
Pavel <black_yaguar@mail.ru>
2024-02-23 00:06:32 +0500

    Создание административной панели для товаров

--- website/online_storeapp/admin.py
+++ website/online_storeapp/admin.py
@@ -34,13 +34,38 @@ class ClientsModelAdmin(admin.ModelAdmin):
 
 @admin.register(GoodsModel)
 class GoodsModelAdmin(admin.ModelAdmin):
-    list_display = ['id','name_product', 'price_of_product','quantity_of_product','date_product_added',]
+    list_display = ['name_product', 'quantity_of_product','date_product_added',]
+    readonly_fields = ['name_product','id','price_of_product','date_product_added',]
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name_product','quantity_of_product',],
+            },
+        ),
+        (
+            'Полная информация товаре',
+            {
+                'classes': ['collapse'],
+                'description': 'Информация о товаре',
+                'fields': ['description_product','price_of_product',],
+            },
+        ),
+         (
+            'Дата заполнения товаром',
+            {
+                'fields': ['date_product_added',],
+            },
+        ),
+        
+    ]
 
 
 @admin.register(OrderstModel)
 class OrderModelAdmin(admin.ModelAdmin):
-    list_display = ['buyer','total_amount_of_order',]
-    readonly_fields = ['goods','id']
+    list_display = ['buyer','total_amount_of_order']
+    readonly_fields = ['date_of_order', 'goods','id','buyer']
     fieldsets = [
         (
             None,
@@ -60,7 +85,7 @@ class OrderModelAdmin(admin.ModelAdmin):
             '№ Заказа',
             {
                 'classes': ['collapse'],
-                'fields': ['id','total_amount_of_order',]
+                'fields': ['id','total_amount_of_order','date_of_order',]
 
             }
         ),
