3ffa5e69c7f087508db5d6aacb354dc5808e0f99
Pavel <black_yaguar@mail.ru>
2024-02-22 20:52:35 +0500

    Создание формы для поиска по какому либо  номеру

--- website/online_storeapp/forms.py
+++ website/online_storeapp/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from .models import GoodsModel, OrderstModel
 
+
 class NewProduct(forms.ModelForm):
     class Meta:
         model = GoodsModel
-        fields = ['name_product','description_product','price_of_product','quantity_of_product', 'image_product']
+        fields = ['name_product', 'description_product',
+                  'price_of_product', 'quantity_of_product', 'image_product']
 
 
 class Buyer_Order_Date(forms.Form):
     num_id = forms.IntegerField(min_value=1)
     days = forms.IntegerField(min_value=7, max_value=365)
+
+
+class NumId(forms.Form):
+    num_id = forms.IntegerField(min_value=1)
