537b4ce17398e678c26a068dec5ff04668f03b21
Pavel <black_yaguar@mail.ru>
2024-02-24 14:24:11 +0500

    Прописал путь form_id

--- website/online_storeapp/urls.py
+++ website/online_storeapp/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from online_storeapp.views import buyer_date, get_goods_all, get_orders_all,index, get_all_orders, order_list, new_product, get_all_orders,description_product, get_clients_all
-
+from . import views
 
 urlpatterns = [
-     path('', index, name='index'),
-     path('orders/<int:name_client>/', get_all_orders, name='orders'),
-     path('order/<int:order_id>/', order_list, name='order'),
-     path('buyer_date/', buyer_date, name='buyer_date'),
-     path('new_product/', new_product, name='new_product'),
-     path('get_all_orders/', get_all_orders, name='get_all_orders'),
-     path('description_product/<int:product_id>', description_product, name='description_product'),
-     path('get_goods_all/', get_goods_all, name='get_goods_all'),
-     path('get_clients_all/', get_clients_all, name='get_clients_all'),
-     path('get_orders_all/', get_orders_all, name='get_orders_all'),
+     path('', views.index, name='index'),
+     path('orders/<int:name_client>/', views.get_all_orders_client, name='orders'),
+     path('order/<int:order_id>/', views.order_list, name='order'),
+     path('buyer_date/', views.buyer_date, name='buyer_date'),
+     path('new_product/', views.new_product, name='new_product'),
+     path('get_all_orders/', views.get_all_orders_client, name='get_all_orders'),
+     path('description_product/<int:product_id>', views.description_product, name='description_product'),
+     path('get_goods_all/', views.get_goods_all, name='get_goods_all'),
+     path('get_clients_all/',views. get_clients_all, name='get_clients_all'),
+     path('get_orders_all/', views.get_orders_all, name='get_orders_all'),
+     path("form_id/", views.search_num, name="form_id"),
     ]
